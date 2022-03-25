4cece3b721f5d1835bd7651a00f0685588c79e62
Dmitry Fedyuk <dfediuk@gmail.com>
2022-03-25 16:51:05 +0300

    https://github.com/ableskills-co-uk/trust-payments/issues/2

--- Plugin/Magento/Sales/Api/Data/OrderInterface.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace SecureTrading\Trust\Plugin\Magento\Sales\Api\Data;
-use Magento\Sales\Api\Data\OrderInterface as Sb;
-# 2022-03-25 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
-# "Implement partial payments": https://github.com/ableskills-co-uk/trust-payments/issues/2
-final class OrderInterface {}
